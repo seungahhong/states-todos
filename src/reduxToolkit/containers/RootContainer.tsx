a62fff3b11a0e692e918ebcf708fa60bc6533c47
hongseungah <gmm117@naver.com>
2022-06-13 22:49:36 +0900

    remove gh-pages

--- src/reduxToolkit/containers/RootContainer.tsx
+++ src/reduxToolkit/containers/RootContainer.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { ErrorBoundary } from 'react-error-boundary';
-import TodoContentContainer from './TodoContentContainer';
+import React from "react";
+import { Provider } from "react-redux";
+import { ErrorBoundary } from "react-error-boundary";
+import TodoContentContainer from "./TodoContentContainer";
 
 import store from "../states/store";
 
@@ -10,14 +10,14 @@ interface FallbackProps {
   resetErrorBoundary: (...args: Array<unknown>) => void;
 }
 
-const ErrorFallback = ({error, resetErrorBoundary}: FallbackProps) => {
+const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
   return (
     <div role="alert">
       <p>Something went wrong:</p>
       <pre>{error.message}</pre>
       <button onClick={resetErrorBoundary}>Try again</button>
     </div>
-  )
+  );
 };
 
 function RootContainer() {
